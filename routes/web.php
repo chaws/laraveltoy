f610d877c9ca2d231704c23382d402c0c575303d
Charles Oliveira <18oliveira.charles@gmail.com>
2017-12-15 16:40:51 -0500

    Adicionadas mais rotas para numero, temperatura e dashboard

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
+Route::get('/property', 'PropertyController@index');
+Route::get('/property/add', 'PropertyController@add');
+Route::post('/property/submit', 'PropertyController@submit');
+
+Route::get('/weather', 'WeatherController@temp');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/dashboard', 'DashboardController@index')->name('home');
