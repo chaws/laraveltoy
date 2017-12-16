67ae4e33084b18a9bee9d5db16a97855921ade67
Charles Oliveira <18oliveira.charles@gmail.com>
2017-12-16 02:29:08 -0500

    Melhorada a organizacao das rotas

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,35 @@
 |
 */
 
-Route::get('/property', 'PropertyController@index');
-Route::get('/property/add', 'PropertyController@add');
-Route::post('/property/submit', 'PropertyController@submit');
-
-Route::get('/weather', 'WeatherController@temp');
-
+/*
+ |  Rota padrao
+ */
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
+/*
+ |  Rotas especificas para o dashboard
+ */
 Route::get('/dashboard', 'DashboardController@index')->name('home');
+
+/*
+ | Rotas para inserir/editar/consultar/deletar Numeros (propriedade)
+ */
+Route::get('/property', 'PropertyController@index');
+Route::get('/property/add', 'PropertyController@add');
+Route::get('/property/edit/{id}', 'PropertyController@edit');
+Route::get('/property/del/{id}', 'PropertyController@del');
+Route::post('/property/submit', 'PropertyController@submit');
+
+/*
+ |  Rota para consultar a temperatura atual de SP
+ */
+Route::get('/weather', 'WeatherController@temp');
+
+/*
+ |  Rota para consultar uma noticia sobre imoveis
+ */
+Route::get('/news/{id}', 'NewsController@get');
